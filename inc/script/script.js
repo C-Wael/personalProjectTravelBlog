cc97d651d89db9aa153feb77a9d5abc8ac367036
C Wael <wael.charni@outlook.fr>
2024-06-30 23:49:19 +0200

    Adding 2 arrays for the imgs of the sliders

--- inc/script/script.js
+++ inc/script/script.js
@@ -1,2 +1,15 @@
 const slideOne = document.getElementById("slideOne");
 const slideTwo = document.getElementById("slideTwo");
+
+const sliderOne = [
+    `<img id="slideOne" src="inc/img/1.jpg" />`,
+    `<img id="slide-2" src="inc/img/2.jpeg" />`,
+    `<img id="slide-3" src="inc/img/3.jpeg" />`
+];
+
+const sliderTwo = [
+    `<img id="slideTwo" src="inc/img/4.jpeg" />`,
+    `<img id="slide-5" src="inc/img/5.jpeg" />`,
+    `<img id="slide-6" src="inc/img/6.jpeg" />`
+];
+
