d7be596052147a9ec1c40cf083785653f5390694
C Wael <wael.charni@outlook.fr>
2024-07-01 23:41:43 +0200

    Adding comments, adding the functions for the left and right buttons of the second slider with the click event listeners.

--- inc/script/script.js
+++ inc/script/script.js
@@ -1,3 +1,4 @@
+// Getting the elements from the DOM.
 const slideOne = document.getElementById("slideOne");
 const slideTwo = document.getElementById("slideTwo");
 const arrowLeftOne = document.getElementById("arrowLeftOne");
@@ -5,7 +6,7 @@ const arrowLeftTwo = document.getElementById("arrowLeftTwo");
 const arrowRightOne = document.getElementById("arrowRightOne");
 const arrowRightTwo = document.getElementById("arrowRightTwo");
 
-
+// Arrays of the srcs for my sliders
 const sliderArrOne = [
     "inc/img/1.jpg",
     "inc/img/2.jpeg",
@@ -18,10 +19,13 @@ const sliderArrTwo = [
     "inc/img/6.jpeg"
 ];
 
+// index for the sliders
 let currentIndexOne = 0;
+let currentIndexTwo = 0;
 
+// First slider left and right buttons
 const imageChangerOneLeft = () => {
-    currentIndexOne = 2;
+    currentIndexOne--;
     if (currentIndexOne < 0) {
         currentIndexOne = sliderArrOne.length - 1;
     }
@@ -36,5 +40,25 @@ const imageChangerOneRight = () => {
     slideOne.src = sliderArrOne[currentIndexOne];
 };
 
+// Second slider left and right buttons
+const imageChangerTwoLeft = () => {
+    currentIndexTwo--;
+    if (currentIndexTwo < 0) {
+        currentIndexTwo = sliderArrTwo.length -1;
+    }
+    slideTwo.src = sliderArrTwo[currentIndexTwo];
+}
+
+const imageChangerTwoRight = () => {
+    currentIndexTwo++;
+    if (currentIndexTwo >= sliderArrTwo.length) {
+        currentIndexTwo = 0;
+    }
+    slideTwo.src = sliderArrTwo[currentIndexTwo];
+};
+
+// Event listeners for the sliders buttons
 arrowLeftOne.addEventListener("click", imageChangerOneLeft);
 arrowRightOne.addEventListener("click", imageChangerOneRight);
+arrowLeftTwo.addEventListener("click", imageChangerTwoLeft);
+arrowRightTwo.addEventListener("click", imageChangerTwoRight);
