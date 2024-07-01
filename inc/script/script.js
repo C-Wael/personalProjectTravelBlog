4bd2a6a4d84bc763b1a9eb1e37ffed6e3b368fb4
C Wael <wael.charni@outlook.fr>
2024-07-01 23:29:24 +0200

    Adding the script tag to the index file and deleting the old img tags, adding the functions for the left and right button to change the images of the first slider, and adding the click event listener to each buttons.

--- inc/script/script.js
+++ inc/script/script.js
@@ -1,15 +1,40 @@
 const slideOne = document.getElementById("slideOne");
 const slideTwo = document.getElementById("slideTwo");
+const arrowLeftOne = document.getElementById("arrowLeftOne");
+const arrowLeftTwo = document.getElementById("arrowLeftTwo");
+const arrowRightOne = document.getElementById("arrowRightOne");
+const arrowRightTwo = document.getElementById("arrowRightTwo");
 
-const sliderOne = [
-    `<img id="slideOne" src="inc/img/1.jpg" />`,
-    `<img id="slide-2" src="inc/img/2.jpeg" />`,
-    `<img id="slide-3" src="inc/img/3.jpeg" />`
+
+const sliderArrOne = [
+    "inc/img/1.jpg",
+    "inc/img/2.jpeg",
+    "inc/img/3.jpeg"
 ];
 
-const sliderTwo = [
-    `<img id="slideTwo" src="inc/img/4.jpeg" />`,
-    `<img id="slide-5" src="inc/img/5.jpeg" />`,
-    `<img id="slide-6" src="inc/img/6.jpeg" />`
+const sliderArrTwo = [
+    "inc/img/4.jpeg",
+    "inc/img/5.jpeg",
+    "inc/img/6.jpeg"
 ];
 
+let currentIndexOne = 0;
+
+const imageChangerOneLeft = () => {
+    currentIndexOne = 2;
+    if (currentIndexOne < 0) {
+        currentIndexOne = sliderArrOne.length - 1;
+    }
+    slideOne.src = sliderArrOne[currentIndexOne];
+}
+
+const imageChangerOneRight = () => {
+    currentIndexOne++;
+    if (currentIndexOne >= sliderArrOne.length) {
+        currentIndexOne = 0;
+    }
+    slideOne.src = sliderArrOne[currentIndexOne];
+};
+
+arrowLeftOne.addEventListener("click", imageChangerOneLeft);
+arrowRightOne.addEventListener("click", imageChangerOneRight);
